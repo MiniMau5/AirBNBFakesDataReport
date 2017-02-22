d9cf572cab9ed70099602483f41a5cc330a216cc
Kemal Ahmed <goatandsheep@gmail.com>
2017-02-22 14:53:10 -0500

    added some changes to the regex

--- scanner.py
+++ scanner.py
@@ -3,22 +3,30 @@ import re
 #identifiers
 ###
 
+
 def findMail(desc):
-    regex = '([\w\.\_])+((\sat\s)|@)(\w)+((\sdot\s)|\.)(\w)+'
+    # defined model by hand
+    # there are plans to develop that using ML in the future
+    regex = '([\w\.\_])+(([\s\_\-]at[\s\_\-])|([\s\_\-])?@([\s\_\-])?)(\w)+(([\s\_\-])|([\s\_\-]dot[\s\_\-])|([\s\_\-])?\.([\s\_\-])?)(\w)+'
     result = re.search(regex, desc)
-    return result.group(0)
+    if result:
+        return result.group(0)
+    return False
 
 # is phone regex even feasible? Probably not
-# def findPhone(desc):
-#     regex = ''
-#     result = re.search(regex, desc)
-#     return result.group(0)
+def findPhone(desc):
+    # regex = ''
+    # result = re.search(regex, desc)
+    # return result.group(0)
+    return False
 
 def isSketchy(desc):
-    if (findMail(desc) || findPhone(desc)):
+    if findMail(desc) or findPhone(desc):
         return True
+    return False
 
-description = ""
+# description = ""
+description = "Contact me mim-at-bnbvacation_com"
 print isSketchy(description)
 
 # attributes
