4efab59afbfd830b632bad17144ba2e3dc08f47c
MiniMau5 <miniriar1@gmail.com>
2017-02-27 14:54:32 -0500

    updated airbnb.py- to pull data and convert json to csv file

--- airbnb.py
+++ airbnb.py
@@ -1,5 +1,8 @@
 import urllib2
 import urllib
+import json
+
+import csv
 
 #from urllib import urlencode
 #import urllib.urlencode
@@ -35,15 +38,41 @@ for i in range(1):
 	print url
 	try:
 		f = urllib2.urlopen(req)
+		x = json.loads(f)
 		# results.append(f.read())
 		# results2=(f.read())
 		# resultsFile.write("open")
 		# resultsFile.write(str(results.pop()))
-		resultsFile.write(str(f.read()))
+		#resultsFile.write(str(f.read()))
 	except:
 		results.append("failure")
 		resultsFile.write("failure")
 print "DONE"
-resultsFile.write("DONE")
+
+hosting_data = x['search_results']
+
+# open a file for writing
+f = csv.writer(open('AirbnbData.csv', 'w'))
+
+# write the headers to csv file
+
+f.writerow(["number", "id", "bathrooms","bedrooms","beds","instant_bookable","is_new_listing", "person_capacity", "property_type", "reviews_count", "room_type"])
+
+# find number of results in json file
+print len(x['search_results'])
+#create counter for tracking in file
+count=0
+
+for key in x['search_results']:
+	count = count +1
+
+	#y is the dict code for each listing retrieved
+	y=key["listing"]
+	# write to each row - important info for each listing - especially id - to be used for further investigation
+	# name and city were not used - as they had accents - which caused UnicodeencodeError - ascii codec can't encode...
+	f.writerow( [count, y["id"], y["bathrooms"],y["bedrooms"], y["beds"],  y["instant_bookable"], y["is_new_listing"], y["person_capacity"], y["property_type"], y["reviews_count"], y["room_type"]])
+print ("finished")
+readFile.close()
+#resultsFile.write("DONE")
 resultsFile.close()
 
